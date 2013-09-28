81df99f7cd60902dc4f1d8c95d79889cf86fd1b6
vladimir.lyukov <v.lyukov@gmail.com>
2013-09-28 20:20:46 +0400

    Added github.worker._get_parser_for_repository

--- wtl/wtgithub/tests/worker.py
+++ wtl/wtgithub/tests/worker.py
@@ -2,25 +2,44 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 from exam.asserts import AssertsMixin
-from github import UnknownObjectException
+from github import Github, UnknownObjectException
 
 from wtl.wtgithub.models import Repository
 from wtl.wtgithub.tests.factories import RepositoryFactory
-from wtl.wtgithub.worker import GithubWorker
+from wtl.wtgithub.worker import GithubWorker, CantFindParserError
 from wtl.wtlib.models import Project
+from wtl.wtparser.parsers import RequirementsParser
 
 
-class AnalyzeRepoTestCase(TestCase, AssertsMixin):
+gh_rep = None
+def setUpModule():
+    global gh_rep
+    github = Github()
+    gh_rep = github.get_repo('elegion/djangodash2013')
+
+
+class BaseTestCase(TestCase):
     def setUp(self):
         self.worker = GithubWorker()
+        super(BaseTestCase, self).setUp()
 
-    def test_called_with_invalid_url(self):
-        with self.assertRaises(UnknownObjectException):
-            self.worker.analyze_repo('invalid/url')
 
+class GetParserForRepositoryTestCase(BaseTestCase):
+    def test_returns_parser(self):
+        parser = self.worker._get_parser_for_repository(gh_rep)
+        self.assertIsInstance(parser, RequirementsParser)
+
+    def test_returns_none(self):
+        github = Github()
+        repo = github.get_repo('github/objective-c-conventions')
+        with self.assertRaises(CantFindParserError):
+            self.worker._get_parser_for_repository(repo)
+
+
+class GetOrCreateRepositoryTestCase(BaseTestCase, AssertsMixin):
     def test_creates_repository(self):
         with self.assertChanges(Repository.objects.count, before=0, after=1):
-            self.worker.analyze_repo('elegion/djangodash2013')
+            self.worker._get_or_create_repository(gh_rep)
 
         repository = Repository.objects.all()[0]
 
@@ -33,7 +52,7 @@ class AnalyzeRepoTestCase(TestCase, AssertsMixin):
                           starsCount=100)
 
         with self.assertDoesNotChange(Repository.objects.count):
-            self.worker.analyze_repo('elegion/djangodash2013')
+            self.worker._get_or_create_repository(gh_rep)
 
         repository = Repository.objects.all()[0]
 
@@ -43,7 +62,7 @@ class AnalyzeRepoTestCase(TestCase, AssertsMixin):
 
     def test_creates_project_if_not_exist(self):
         with self.assertChanges(Project.objects.count, before=0, after=1):
-            self.worker.analyze_repo('elegion/djangodash2013')
+            self.worker._get_or_create_repository(gh_rep)
 
         project = Project.objects.all()[0]
 
@@ -57,10 +76,16 @@ class AnalyzeRepoTestCase(TestCase, AssertsMixin):
                           starsCount=100)
 
         with self.assertChanges(Project.objects.count, before=0, after=1):
-            self.worker.analyze_repo('elegion/djangodash2013')
+            self.worker._get_or_create_repository(gh_rep)
 
         project = Project.objects.all()[0]
 
         self.assertEqual('djangodash2013', project.name)
         self.assertEqual('djangodash2013', project.github.name)
         self.assertEqual('elegion', project.github.owner)
+
+
+class AnalyzeRepoTestCase(BaseTestCase):
+    def test_called_with_invalid_url(self):
+        with self.assertRaises(UnknownObjectException):
+            self.worker.analyze_repo('invalid/url')
--- wtl/wtgithub/worker.py
+++ wtl/wtgithub/worker.py
@@ -3,6 +3,21 @@ from github import Github
 
 from wtl.wtgithub.models import Repository
 from wtl.wtlib.models import Project
+from wtl.wtparser.parser import get_parser_for_filename
+
+
+class WorkerError(BaseException):
+    """
+    Base class for all worker exceptions
+    """
+    pass
+
+
+class CantFindParserError(WorkerError):
+    """
+    Raised when can't find requirements file in given repository
+    """
+    pass
 
 
 class GithubWorker(object):
@@ -16,9 +31,12 @@ class GithubWorker(object):
         if github is not None:
             self.github = github
 
-    def analyze_repo(self, full_name):
-        rep = self.github.get_repo(full_name)
-
+    def _get_or_create_repository(self, rep):
+        """
+        Fetches github repository information, creates `wtgithub.models.Repository`
+        and `wtlib.models.Project` if does not exist.
+        Returns github repository information and `wtgithub.models.Repository`
+        """
         try:
             repository = Repository.objects.get(name=rep.name,
                                                 owner=rep.owner._identity)
@@ -31,4 +49,24 @@ class GithubWorker(object):
             repository.project
         except Project.DoesNotExist:
             Project.objects.create(github=repository, name=rep.name)
-        return repository
+        return rep, repository
+
+    def _get_parser_for_repository(self, rep):
+        """
+        Analyzes repository tree to find corresponding requirements parser.
+        Currently analyzes only repository root and returns first matching parser.
+        Raises CantFindParserError if can't determine parser for given repository.
+        """
+        tree = rep.get_git_tree('master')
+        for node in tree.tree:
+            if node.type != 'blob':
+                continue
+            parser = get_parser_for_filename(node.path)
+            if parser is not None:
+                return parser
+        raise CantFindParserError()
+
+    def analyze_repo(self, full_name):
+        rep = self.github.get_repo(full_name)
+        repository = self._get_or_create_repository(rep)
+        parser = self._get_parser_for_repository(rep)
