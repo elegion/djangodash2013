e1e4528677ec884c70e0dfaf917fd7d72b338393
Dmitry Medvinsky <me@dmedvinsky.name>
2013-09-28 22:52:33 +0400

    Fix factories and, thus, failing tests

--- wtl/wtlib/factories.py
+++ wtl/wtlib/factories.py
@@ -1,11 +1,18 @@
 from __future__ import unicode_literals
 
 import factory
+
 from . import models
+
+
+class LanguageFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = models.Language
+    name = factory.Sequence(lambda n: 'lang {0}'.format(n))
 
 
 class LibraryFactory(factory.DjangoModelFactory):
     FACTORY_FOR = models.Library
+    language = factory.SubFactory(LanguageFactory)
     name = 'mylib'
 
 
