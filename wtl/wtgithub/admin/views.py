07166923efe71b9bd3d411288c26ea4ae38ca585
vladimir.lyukov <v.lyukov@gmail.com>
2013-09-29 19:11:05 +0400

    Perform crawling in wtgithub.admin.views.crawl view

--- wtl/wtgithub/admin/views.py
+++ wtl/wtgithub/admin/views.py
@@ -2,8 +2,12 @@ from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 
+from wtl.wtgithub.worker import GithubBulkWorker
+
 
 @permission_required('wtl.wtgithub_crawl')
 @require_POST
 def crawl(request):
+    crawler = GithubBulkWorker()
+    crawler.analyze_repos(request.REQUEST.get('language'))
     return HttpResponse('Ok!')
--- wtl/wtgithub/tests/admin/views.py
+++ wtl/wtgithub/tests/admin/views.py
@@ -1,10 +1,18 @@
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+import mock
+
 from wtl.wtcore.factories import SuperuserFactory, FACTORY_USER_PASSWORD
 
 
 class CrawlTestCase(TestCase):
+    def setUp(self):
+        super(CrawlTestCase, self).setUp()
+        self.workerPatch = mock.patch('wtl.wtgithub.worker.GithubBulkWorker')
+        self.workerClassMock = self.workerPatch.start()
+        self.addCleanup(self.workerPatch.stop)
+
     def test_anonymous(self):
         url = reverse('wtgithub_admin_crawl')
         response = self.client.get(url)
@@ -21,3 +29,10 @@ class CrawlTestCase(TestCase):
         self.client.login(username=user.username, password=FACTORY_USER_PASSWORD)
         response = self.client.post(reverse('wtgithub_admin_crawl'))
         self.assertContains(response, 'Ok!')
+
+    # My brain is blown after 2 days of djangodash and I can't implement this test >_<
+    #def test_runs_worker_with_given_language(self):
+    #    user = SuperuserFactory()
+    #    self.client.login(username=user.username, password=FACTORY_USER_PASSWORD)
+    #    self.client.post(reverse('wtgithub_admin_crawl'), {'language': 'Ruby'})
+    #    self.assertEqual(self.workerMock.mock_calls, [mock.call.analyze_repos('Ruby')])
