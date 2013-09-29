be3053df1e653858f9e477101d86fcf1d01ff471
vladimir.lyukov <v.lyukov@gmail.com>
2013-09-30 00:59:48 +0400

    Fixed ThreadWithReturnValue to always return tuple

--- wtl/streaming_log_response/thread.py
+++ wtl/streaming_log_response/thread.py
@@ -20,7 +20,10 @@ if sys.version_info >= (3,):
 
         def join(self, *args, **kwargs):
             Thread.join(self, *args, **kwargs)
-            return self._return
+            if self._return is None:
+                return ()
+            else:
+                return self._return
 else:
     class ThreadWithReturnValue(Thread):
         def __init__(self, *args, **kwargs):
@@ -31,6 +34,10 @@ else:
             if self._Thread__target is not None:
                 self._return = self._Thread__target(*self._Thread__args,
                                                     **self._Thread__kwargs)
+
         def join(self, *args, **kwargs):
             Thread.join(self, *args, **kwargs)
-            return self._return
+            if self._return is None:
+                return ()
+            else:
+                return self._return
