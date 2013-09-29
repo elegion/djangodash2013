9825d76b6bff7e04d1e277539ece6bda928b648b
Dmitry Medvinsky <me@dmedvinsky.name>
2013-09-29 15:47:21 +0400

    Validate repo exists in `AnalyzeForm`

--- wtl/wtlib/forms.py
+++ wtl/wtlib/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.forms.util import ErrorList
+from django.utils.translation import ugettext_lazy as _
+from github import UnknownObjectException
 
 from wtl.wtgithub.worker import GithubWorker
 
@@ -7,6 +10,12 @@ class AnalyzeForm(forms.Form):
     git_url = forms.CharField()
 
     def analyze(self):
+        if 'git_url' not in self._errors:
+            self._errors['git_url'] = ErrorList()
         worker = GithubWorker()
-        self.repository, self.project = worker.analyze_repo(self.cleaned_data['git_url'])
-        return self.repository, self.project
+        try:
+            self.repository, self.project = worker.analyze_repo(
+                self.cleaned_data['git_url'])
+            return self.repository, self.project
+        except UnknownObjectException:
+            self._errors['git_url'].append(_('Repository not found.'))
