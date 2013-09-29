d71fa5145b0ba8c53c87dfb7fc8242a1e8530071
vladimir.lyukov <v.lyukov@gmail.com>
2013-09-29 17:06:37 +0400

    Removed unused CustomAppIndexDashboard

--- wtl/admin_dashboard.py
+++ wtl/admin_dashboard.py
@@ -13,7 +13,7 @@ And to activate the app index dashboard::
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
 
-from admin_tools.dashboard import modules, Dashboard, AppIndexDashboard
+from admin_tools.dashboard import modules, Dashboard
 from admin_tools.utils import get_admin_site_name
 
 
@@ -52,31 +52,3 @@ class CustomIndexDashboard(Dashboard):
 
         # append a recent actions module
         self.children.append(modules.RecentActions(_('Recent Actions'), 5))
-
-
-class CustomAppIndexDashboard(AppIndexDashboard):
-    """
-    Custom app index dashboard for wtl.
-    """
-
-    # we disable title because its redundant with the model list module
-    title = ''
-
-    def __init__(self, *args, **kwargs):
-        AppIndexDashboard.__init__(self, *args, **kwargs)
-
-        # append a model list module and a recent actions module
-        self.children += [
-            modules.ModelList(self.app_title, self.models),
-            modules.RecentActions(
-                _('Recent Actions'),
-                include_list=self.get_app_content_types(),
-                limit=5
-            )
-        ]
-
-    def init_with_context(self, context):
-        """
-        Use this method if you need to access the request context.
-        """
-        return super(CustomAppIndexDashboard, self).init_with_context(context)
